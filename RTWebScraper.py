b795ae40fe518488f270be2b46c70e2227d1eedb
Femi Kalejaiye <femikalejaiye@Femis-MacBook-Pro.local>
2020-04-06 21:23:53 -0500

    Started Working on code to get all the urls for each movie

--- RTWebScraper.py
+++ RTWebScraper.py
@@ -9,16 +9,32 @@ client = MongoClient()
 rotten_tomatoes_db=client['rotten_tomatoes']
 rating = rotten_tomatoes_db['ratings']
 
-url = 
+url_main = 'https://www.rottentomatoes.com/browse/dvd-streaming-all/'
+urls= []
 
-r = requests.get(url)
+q = requests.get(url_main)
+ 
+soup = BeautifulSoup(r.text,'html.parser')
 
-whole_page= rotten_tomatoes_db['whole_page']
-whole_page.insert_one({'html': r.content, 'url':url, 'time_scraped':time.time()})
+link = soup.find()
+for tag in link:
+    urls.append(tag.text)
 
-soup = BeautifulSoup(r.text, 'html.parser')
 
-movie = soup.find('h1', class_="mop-ratings-wrap__title mop-ratings-wrap__title--top").text
-tomatometer = soup.find('span', class_="mop-ratings-wrap__percentage").text
-audience
-ratings.insert_one({'movie':movie, 'tomatometer':tomatometer, 'audience':audience})
+for url in urls:
+
+    r = requests.get(url)
+
+    whole_page= rotten_tomatoes_db['whole_page']
+    whole_page.insert_one({'html': r.content, 'url':url, 'time_scraped':time.time()})
+
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    movie = soup.find('h1', class_="mop-ratings-wrap__title mop-ratings-wrap__title--top").text
+    tomatometer,audience = [i for i in soup.find_all('span', class_="mop-ratings-wrap__percentage")]
+    tomatometer = tomatometer.strip('\n').strip().strip('%')
+    audience = audience.strip('\n').strip().strip('%')
+    date = soup.find('time').text
+    ratings.insert_one({'movie':movie, 'tomatometer':tomatometer, 'audience':audience, 'date':date})
+
+    time(5)
