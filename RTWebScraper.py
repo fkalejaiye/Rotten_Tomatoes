4aa91733e50074762d63ef05dbec5c77f77a8ca4
Femi Kalejaiye <femikalejaiye@Femis-MacBook-Pro.local>
2020-04-07 09:48:50 -0500

    Fixed typos in intro and cleaned up web scraper

--- RTWebScraper.py
+++ RTWebScraper.py
@@ -10,31 +10,42 @@ rotten_tomatoes_db=client['rotten_tomatoes']
 rating = rotten_tomatoes_db['ratings']
 
 url_main = 'https://www.rottentomatoes.com/browse/dvd-streaming-all/'
-urls= []
+urls=[]
 
-q = requests.get(url_main)
+def get_urls(url_main):
+
+    q = requests.get(url_main)
  
-soup = BeautifulSoup(r.text,'html.parser')
+    soup = BeautifulSoup(r.text,'html.parser')
 
-link = soup.find()
-for tag in link:
-    urls.append(tag.text)
+    link = soup.find()
+    for tag in link:
+        urls.append(tag.text)
+    
 
 
-for url in urls:
+def get_info_from_urls(urls)
+    for url in urls:
 
-    r = requests.get(url)
+        r = requests.get(url)
 
-    whole_page= rotten_tomatoes_db['whole_page']
-    whole_page.insert_one({'html': r.content, 'url':url, 'time_scraped':time.time()})
+        whole_page= rotten_tomatoes_db['whole_page']
+        whole_page.insert_one({'html': r.content, 'url':url, 'time_scraped':time.time()})
 
-    soup = BeautifulSoup(r.text, 'html.parser')
+        soup = BeautifulSoup(r.text, 'html.parser')
 
-    movie = soup.find('h1', class_="mop-ratings-wrap__title mop-ratings-wrap__title--top").text
-    tomatometer,audience = [i for i in soup.find_all('span', class_="mop-ratings-wrap__percentage")]
-    tomatometer = tomatometer.strip('\n').strip().strip('%')
-    audience = audience.strip('\n').strip().strip('%')
-    date = soup.find('time').text
-    ratings.insert_one({'movie':movie, 'tomatometer':tomatometer, 'audience':audience, 'date':date})
+        movie = soup.find('h1', class_="mop-ratings-wrap__title mop-ratings-wrap__title--top").text
+        tomatometer,audience = [i for i in soup.find_all('span', class_="mop-ratings-wrap__percentage")]
+        tomatometer = tomatometer.strip('\n').strip().strip('%')
+        audience = audience.strip('\n').strip().strip('%')
+        date = soup.find('time').text
+        ratings.insert_one({'movie':movie, 'tomatometer':tomatometer, 'audience':audience, 'date':date})
 
-    time(5)
+        time(5)
+
+if __name__ == '__main__':
+	#run scraper and print completion time
+	print('Running')
+	start = time.time()
+	end = time.time() - start
+	print("Completed, time: " + str(end) + " secs")
